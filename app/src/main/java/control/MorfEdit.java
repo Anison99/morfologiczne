a53671b16cf0bcbfee9e3f9714fa548506ec5d57
anison99_ <anita.zych9941@gmail.com>
2022-05-03 16:20:08 +0200

    refactor project

--- app/src/main/java/control/MorfEdit.java
+++ app/src/main/java/control/MorfEdit.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MorfEdit extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MorfEdit.class.getResource("MorfEdit.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        stage.setResizable(false);
+        stage.setTitle("TEST TEST");
         stage.setScene(scene);
         stage.show();
     }
