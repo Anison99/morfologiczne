0f34589b4471060b01d43daf0506924cd78af25a
Arek Nowak <Fason4@wp.pl>
2022-05-03 17:24:21 +0200

    Repaired bug

--- app/src/main/java/control/MorfEdit.java
+++ app/src/main/java/control/MorfEdit.java
@@ -2,24 +2,33 @@ package control;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MorfEdit extends Application {
+    private static Scene scene;
     @Override
     public void start(Stage stage) throws IOException {
-       /* FXMLLoader fxmlLoader = new FXMLLoader(MorfEdit.class.getResource("MorfEdit.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        */
-        stage.setResizable(false);
-        stage.setTitle("TEST TEST");
-        //stage.setScene(scene);
+        scene = new Scene(loadFXML("MorfEdit"), 640, 480);
+        stage.setScene(scene);
         stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MorfEdit.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
         launch();
     }
+
 }
