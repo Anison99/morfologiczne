889b5843a969c57bac6d9b4646cdfafd007cb400
anison99_ <anita.zych9941@gmail.com>
2022-05-03 16:55:21 +0200

    front 1

--- app/src/main/java/control/MorfEdit.java
+++ app/src/main/java/control/MorfEdit.java
@@ -11,7 +11,7 @@ public class MorfEdit extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MorfEdit.class.getResource("MorfEdit.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setResizable(false);
         stage.setTitle("TEST TEST");
         stage.setScene(scene);
