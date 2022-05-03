aba9820606f703580acfbad0aa0036f1ba8a3ce9
Szczepan Wójcik <szczep1000@outlook.com>
2022-05-03 23:32:04 +0200

    Remove modular nature of Project

--- app/src/main/java/control/MorfEdit.java
+++ app/src/main/java/control/MorfEdit.java
@@ -1,37 +1,35 @@
 package control;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class MorfEdit extends Application {
-    private static Scene scene;
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("MorfEdit"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+	private static Scene scene;
 
+	@Override
+	public void start(Stage stage) throws IOException {
+		scene = new Scene(loadFXML("MorfEdit"), 640, 480);
+		stage.setScene(scene);
+		stage.show();
 
+	}
 
-    }
+	static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml));
+	}
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+	private static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(MorfEdit.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MorfEdit.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+		launch();
+	}
 
 }
