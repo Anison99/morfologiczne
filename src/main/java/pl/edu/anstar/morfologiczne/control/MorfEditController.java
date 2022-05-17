049e47a2435b6fa9c048d2a2da187f504f0c1ed4
Szczepan Wójcik <szczep1000@outlook.com>
2022-05-17 19:47:20 +0200

    Remove outdated methods

--- src/main/java/pl/edu/anstar/morfologiczne/control/MorfEditController.java
+++ src/main/java/pl/edu/anstar/morfologiczne/control/MorfEditController.java
@@ -13,14 +13,16 @@ import javafx.scene.image.ImageView;
 @SuppressWarnings("unused")
 public class MorfEditController implements Initializable {
 	
+	// Children
+	@FXML
+	private RadioGroupController radioGroupController;
+	
 	@FXML
 	ChoiceBox<?> choiceFcnList;
 	@FXML
 	ChoiceBox<String> operationList;
 	@FXML
 	Label iterLabel;
-	
-	final String[] types = { "Szkieletyzacja", "Obcinanie gałęzi", "Centroidy" };
 	
 	// Image handling
 	@FXML
@@ -34,66 +36,12 @@ public class MorfEditController implements Initializable {
 		
 	}
 	
-	@FXML
-	private RadioGroupController radioGroupController;
 	// ----- METHODS
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		radioGroupController.setParentController(this);
 		
-//		ToggleGroup languageRadioToggleGroup = new ToggleGroup();
-//		this.languagePLRadio.setSelected(true);
-//		this.languagePLRadio.setToggleGroup(languageRadioToggleGroup);
-//		this.languageENGRadio.setToggleGroup(languageRadioToggleGroup);
 	}
 	
-	public void buttonOption() {
-		String typeChoose = operationList.getValue();
-		typOption(typeChoose);
-	}
-	
-	private void typOption(String typeChoose) {
-		String chooseLabel = "Obecny Format: " + typeChoose;
-		int op1 = updateOp(typeChoose);
-		switch (typeChoose) {
-		case "Rzymski":
-			/*
-			 * date.setOperator(op1); dateLabel1.setText(chooseLabel); update();
-			 */
-			
-		case "Krótki":
-			/*
-			 * date.setOperator(op1); dateLabel1.setText(chooseLabel); update();
-			 * 
-			 */
-		case "Długi":
-			/*
-			 * date.setOperator(op1); dateLabel1.setText(chooseLabel); update();
-			 * 
-			 */
-		case "Alternatywny":
-			/*
-			 * date.setOperator(op1); dateLabel1.setText(chooseLabel); update();
-			 * 
-			 */
-		case "Domyślny":
-			/*
-			 * date.setOperator(op1); dateLabel1.setText(chooseLabel); update();
-			 * 
-			 */
-		}
-	}
-	
-	private int updateOp(String string) {
-		switch (string) {
-		case "Szkieletyzacja":
-			return 3;
-		case "Obcinanie gałęzi":
-			return 2;
-		case "Centroidy":
-			return 1;
-		}
-		return 4;
-	}
 }
