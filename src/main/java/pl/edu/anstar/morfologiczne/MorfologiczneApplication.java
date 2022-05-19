7ecbd1c9080e60b799a29b7e915ed19784c756a2
Szczepan Wójcik <szczep1000@outlook.com>
2022-05-19 11:21:57 +0200

    Add size change listener

--- src/main/java/pl/edu/anstar/morfologiczne/MorfologiczneApplication.java
+++ src/main/java/pl/edu/anstar/morfologiczne/MorfologiczneApplication.java
@@ -7,6 +7,7 @@ import org.opencv.core.Core;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MorfologiczneApplication extends Application {
@@ -18,14 +19,27 @@ public class MorfologiczneApplication extends Application {
 		currentScene = FXMLLoader.load(getClass().getResource("control/MorfEdit.fxml"));
 //		stage.setWidth(1920);
 //		stage.setHeight(1080);
-		stage.setMaximized(true);
+//		stage.setMaximized(true);
 		stage.setScene(currentScene);
 		stage.setTitle("Iteracyjne Operacje Morfologiczne");
 		stage.show();
+		letterbox(currentScene, (Pane) currentScene.getRoot());
+		
 	}
 	
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		launch();
 	}
+	
+	private void letterbox(final Scene scene, final Pane contentPane) {
+		final double initWidth = scene.getWidth();
+		final double initHeight = scene.getHeight();
+		final double ratio = initWidth / initHeight;
+		SceneSizeChangeListener sizeListener = new SceneSizeChangeListener(scene, ratio, initHeight, initWidth,
+				contentPane);
+		scene.widthProperty().addListener(sizeListener);
+		scene.heightProperty().addListener(sizeListener);
+	}
+	
 }
