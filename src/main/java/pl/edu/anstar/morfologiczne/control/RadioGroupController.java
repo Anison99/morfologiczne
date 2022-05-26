21b6bf01eab8a233044bf4d32cf33985152e0f8c
Szczepan Wójcik <szczep1000@outlook.com>
2022-05-26 23:55:12 +0200

    Fix bug with choice box initialization
    Choice box on launch was not populated with values

--- src/main/java/pl/edu/anstar/morfologiczne/control/RadioGroupController.java
+++ src/main/java/pl/edu/anstar/morfologiczne/control/RadioGroupController.java
@@ -4,8 +4,6 @@ import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -29,7 +27,6 @@ public class RadioGroupController implements Initializable {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		this.bundle = resources;
 		ToggleGroup languageRadioToggleGroup = new ToggleGroup();
 		this.languagePLRadio.setToggleGroup(languageRadioToggleGroup);
 		this.languageENGRadio.setToggleGroup(languageRadioToggleGroup);
@@ -46,37 +43,15 @@ public class RadioGroupController implements Initializable {
 		}
 	}
 	
-	// public boolean isEnglishSet() {
-	// return (languageENGRadio.isSelected() && !languagePLRadio.isSelected()) ?
-	// true : false;
-	// }
-	//
-	// public boolean isPolishSet() {
-	// return (languagePLRadio.isSelected() && !languageENGRadio.isSelected()) ?
-	// true : false;
-	// }
-	
-	private ResourceBundle bundle;
-	
-	public ResourceBundle getBundle() {
-		return this.bundle;
-	}
-	
-	private void setLanguage(String langCode, String countryCode) {
+	public void setLanguage(String langCode, String countryCode) {
 		Locale locale = new Locale(langCode, countryCode);
-		bundle = ResourceBundle.getBundle("pl.edu.anstar.morfologiczne.internationalization.lang",
-				locale);
+		ResourceBundle bundle = MorfologiczneApplication.setBundle(locale);
 		final String selectImage = bundle.getString("key.selectImage");
 		final String reset = bundle.getString("key.reset");
-		final String skel = bundle.getString("key.skel");
-		final String centroid = bundle.getString("key.centroid");
-		final String branch = bundle.getString("key.branch");
 		parentController.selectImageButton.setText(selectImage);
 		parentController.resetButton.setText(reset);
-		
-		ObservableList<String> choices = FXCollections.observableArrayList(skel, centroid, branch);
-		parentController.algorithmChoiceBox.setItems(choices);
-		parentController.algorithmChoiceBox.setValue(skel);
+		parentController.setUpChoiceBox();
 		MorfologiczneApplication.updateStageTitle(bundle);
 	}
+	
 }
