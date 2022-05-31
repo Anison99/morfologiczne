93c8e7f6741a1f2594337a19ed5ba211bb16662a
Szczepan Wójcik <szczep1000@outlook.com>
2022-05-31 23:23:14 +0200

    Restored test skeleton functionality

--- src/main/java/pl/edu/anstar/morfologiczne/algorithm/Skeletonization.java
+++ src/main/java/pl/edu/anstar/morfologiczne/algorithm/Skeletonization.java
@@ -9,39 +9,38 @@ import org.opencv.imgproc.Imgproc;
 public class Skeletonization extends MorphologicalOperation {
 	
 	@Override
-	public Mat calculate(Mat source) {
+	public Mat calculate(Mat input) {
+		Mat source = input.clone();
 		try {
-			// TODO
-			source.convertTo(source, CvType.CV_8UC3);
-			// Imgproc.threshold(source, source, 127, 255, CvType.CV_8UC3);
-			
+			Imgproc.cvtColor(source, source, Imgproc.COLOR_RGB2GRAY);
+			// source.convertTo(source, CvType.CV_8UC1);
+			// Imgproc.threshold(source, source, 127, 255, CvType.CV_8UC1);
+			// Imgproc.morphologyEx(source, source, Imgproc.MORPH_DILATE, source);
 			int size = source.cols() * source.rows();
 			Size size2 = new Size(source.cols(), source.rows());
-			var skel = Mat.zeros(size2, CvType.CV_8UC3);
+			var skel = Mat.zeros(size2, CvType.CV_8UC1);
+			
+			// Mat kernel = new Mat(3, 3, CvType.CV_16S);
 			var kernel = Imgproc.getStructuringElement(Imgproc.MORPH_CROSS, new Size(3, 3));
+			kernel.convertTo(kernel, CvType.CV_8UC1);
 			boolean done = false;
-			var eroded = Mat.zeros(size2, CvType.CV_8UC3);
-			var temp = Mat.zeros(size2, CvType.CV_8UC3);
+			var eroded = Mat.zeros(size2, CvType.CV_8UC1);
+			var temp = Mat.zeros(size2, CvType.CV_8UC1);
 			while (!done) {
-				System.out.println("1");
-				debug(skel, temp);
+				// Imgproc.morphologyEx(source, eroded, Imgproc.MORPH_ERODE, kernel);
 				
 				Imgproc.erode(source, eroded, kernel);
-				
-				System.out.println("initial");
-				debug(skel, temp);
-				
+				// Imgproc.morphologyEx(eroded, temp, Imgproc.MORPH_DILATE, kernel);
 				Imgproc.dilate(eroded, temp, kernel);
-				System.out.println("after dilation");
-				debug(skel, temp);
 				
 				Core.subtract(source, temp, temp);
-				System.out.println("after subtraction");
-				debug(skel, temp);
 				
 				Core.bitwise_or(skel, temp, skel);
+				
 				System.out.println("Dupa");
+				
 				eroded.copyTo(source);
+				source.convertTo(source, CvType.CV_8UC1);
 				var zeros = size - Core.countNonZero(source);
 				if (zeros == size) {
 					done = true;
@@ -50,20 +49,15 @@ public class Skeletonization extends MorphologicalOperation {
 			return skel;
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			System.out.println(e.getCause());
 			return null;
 		}
 		
 	}
 	
+	@SuppressWarnings("unused")
 	private void debug(Mat skel, Mat temp) {
-		System.out.println("Skel:");
 		System.out.println(skel);
-		System.out.println("Skel size");
-		System.out.println(skel.size());
-		System.out.println("Temp:");
 		System.out.println(temp);
-		System.out.println(temp.size());
 	}
 	
 }
